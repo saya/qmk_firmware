ac5a41f1af699f8a19c73f7fefb1e5e8f27e7185
saya <saya.otanashi.1337@gmail.com>
2022-09-17 19:09:51 +0200

    Re-add OLED

--- keyboards/crkbd/keymaps/saya-shaba/keymap.c
+++ keyboards/crkbd/keymaps/saya-shaba/keymap.c
@@ -26,48 +26,46 @@ enum layers {
     _R_NAV
 };
 
-#define GER_AE   US_ADIA
-#define GER_OE   US_ODIA
-#define GER_UE   US_UDIA
-#define GER_SS   US_SS
+#define GER_AE US_ADIA
+#define GER_OE US_ODIA
+#define GER_UE US_UDIA
+#define GER_SS US_SS
 
-#define  LALT_A MT(MOD_LALT, KC_A)
-#define  LCTL_R MT(MOD_LCTL, KC_R)
-#define  LSFT_S MT(MOD_LSFT, KC_S)
-#define  RSYM_D LT(_R_SYM, KC_D)
-
-#define  LNUM_H LT(_L_NUM, KC_H)
-#define  RSFT_E MT(MOD_RSFT, KC_E)
-#define  RSFT_DOT MT(MOD_RSFT, KC_DOT)
-#define  RCTL_I MT(MOD_RCTL, KC_I)
-#define  LALT_O MT(MOD_LALT, KC_O)
-
-#define LGUI_C LGUI_T(KC_C)
-#define LGUI_COMM LGUI_T(KC_COMM)
-
+#define LALT_A LALT_T(KC_A)
+#define LCTL_R LCTL_T(KC_R)
+#define LSFT_S LSFT_T(KC_S)
+#define RSYM_D LT(_R_SYM, KC_D)
 #define RINTL_T LT(_R_INTL, KC_T)
-#define LINTL_N LT(_L_INTL, KC_N)
-
+#define LSFT_X LSFT_T(KC_X)
+#define LGUI_C LGUI_T(KC_C)
 #define NAV_SPC LT(_R_NAV, KC_SPACE)
 
+#define LINTL_N LT(_L_INTL, KC_N)
+#define RSFT_E RSFT_T(KC_E)
+#define RCTL_I RCTL_T(KC_I)
+#define LALT_O LALT_T(KC_O)
+#define LNUM_H LT(_L_NUM, KC_H)
+#define LGUI_COMM LGUI_T(KC_COMM)
+#define RSFT_DOT RSFT_T(KC_DOT)
+#define SFT_BSPC RSFT_T(KC_BSPC)
 #define FUN_DEL LT(_L_FUN, KC_DEL)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK_DH] = LAYOUT_split_3x6_3(
-                                                                                               /*  ; :       _ */
-        KC_INS,   KC_Q,   KC_W,   KC_F,    KC_P,   KC_B,       KC_J,    KC_L,      KC_U,     KC_Y, KC_SCLN,  KC_UNDS,
-    /* -                                                                                                     ' */
-       KC_MINS, LALT_A, LCTL_R, LSFT_S, RINTL_T,   KC_G,       KC_M, LINTL_N,    RSFT_E,   RCTL_I,  LALT_O,  KC_QUOT,
-      KC_ENTER,   KC_Z,   KC_X, LGUI_C,  RSYM_D,   KC_V,       KC_K,  LNUM_H, LGUI_COMM, RSFT_DOT, KC_SLSH, KC_ENTER,
-                                KC_ESC, NAV_SPC, KC_TAB,     KC_ENT, KC_BSPC,   FUN_DEL
+                                                                                                /*  ; :       _   */
+        KC_INS,   KC_Q,   KC_W,   KC_F,    KC_P,   KC_B,       KC_J,     KC_L,      KC_U,     KC_Y, KC_SCLN,  KC_UNDS,
+    /* -                                                                                                      ' " */
+       KC_MINS, LALT_A, LCTL_R, LSFT_S, RINTL_T,   KC_G,       KC_M,  LINTL_N,    RSFT_E,   RCTL_I,  LALT_O,  KC_QUOT,
+      KC_ENTER,   KC_Z, LSFT_X, LGUI_C,  RSYM_D,   KC_V,       KC_K,   LNUM_H, LGUI_COMM, RSFT_DOT, KC_SLSH, KC_ENTER,
+                                KC_ESC, NAV_SPC, KC_TAB,     KC_ENT, SFT_BSPC,   FUN_DEL
     ),
     [_L_NUM] = LAYOUT_split_3x6_3(
             /*  %      ,                   # */
       _______,  KC_PERC, KC_7, KC_8,   KC_9, KC_HASH,  _______, _______, _______, _______, _______, _______,
-            /*  =                            *                                             : */
+            /*   =                           *                                             : */
       _______,   KC_EQL, KC_4, KC_5,   KC_6, KC_ASTR,  _______, _______, _______, _______, KC_COLN, _______,
-            /*  @                            + */
+            /*    @                          + */
       _______,    KC_AT, KC_1, KC_2,   KC_3, KC_PLUS,  _______, _______, _______, _______, _______, _______,
                                KC_0, KC_SPC, _______,  _______, _______, _______
     ),
@@ -106,3 +104,78 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master()) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  }
+  return rotation;
+}
+
+char keylog_str[24] = {};
+
+const char code_to_name[60] = {
+    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
+    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+void set_keylog(uint16_t keycode, keyrecord_t *record) {
+  char name = ' ';
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
+  if (keycode < 60) {
+    name = code_to_name[keycode];
+  }
+
+  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
+           record->event.key.row, record->event.key.col,
+           keycode, name);
+}
+
+void oled_render_keylog(void) {
+    oled_write(keylog_str, false);
+}
+
+void render_bootmagic_status(bool status) {
+    /* Show Ctrl-Gui Swap options */
+    static const char PROGMEM logo[][2][3] = {
+        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+    };
+    if (status) {
+        oled_write_ln_P(logo[0][0], false);
+        oled_write_ln_P(logo[0][1], false);
+    } else {
+        oled_write_ln_P(logo[1][0], false);
+        oled_write_ln_P(logo[1][1], false);
+    }
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+        0};
+    oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_render_keylog();
+    } else {
+        oled_render_logo();
+    }
+    return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
+  }
+  return true;
+}
+#endif // OLED_ENABLE
